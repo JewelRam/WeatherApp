2275e2d90a59ab5f9bd7cc46fd7c624170b987ad
Jewel Ramirez <chimpy4u@yahoo.com>
2019-01-21 13:45:08 -0800

    it works!

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,39 +1,93 @@
- class Test {
-            constructor() {
-                this.testResults = document.getElementsByClassName('test-results');
-            }
+const appKey = "25e989bd41e3e24ce13173d8126e0fd6";
 
-            async run() {
-                console.log(new Date().toISOString(), '[Test]', 'Running the test');
+//referencing html elements to use in javascript
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
+let descript = document.getElementById("descript");
 
-                // TODO: Make the API call and handle the results
-            }
+// added event listeners to seach and on an "enter" click
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
 
-            setError(message) {
-                // TODO: Format the error
-                this.testResults.innerHTML = (message || '').toString();
-            }
+//funciton that loads the info once the search is entered
+function enterPressed(event) {
+  if (event.key === "Enter") {
+    findWeatherDetails();
+  }
+}
 
-            setResults(results) {
-                // TODO: Format the results
-                this.testResults.innerHTML = (results || '').toString();
-            }
-        }
+function findWeatherDetails() {
+  if (searchInput.value === "") {
+  
+  }else {
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
+   httpRequestAsync(searchLink, theResponse);
+  }
+ }
 
-           function addButtonForTest(context, test) {
-            let testButton = document.createElement('button');
+ //sets the info from the api call to the info searched
+function theResponse(response) {
+  let jsonObject = JSON.parse(response);
+  cityName.innerHTML = jsonObject.name;
+  icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+  temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+  humidity.innerHTML = jsonObject.main.humidity + "%";
+  descript.innerHTML = jsonObject.weather[0].description;
+}
 
-            testButton.type = 'button';
-            testButton.innerText = 'Get the Nashville Weather';
-            testButton.onclick = () => test.run();
+function httpRequestAsync(url, callback)
+{
+  console.log("hello");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => { 
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.send();
+}
+//  class Test {
+//             constructor() {
+//                 this.testResults = document.getElementsByClassName('test-results');
+//             }
 
-            context.appendChild(testButton);
+//             async run() {
+//                 console.log(new Date().toISOString(), '[Test]', 'Running the test');
 
-            return testButton;
-        }
+//                 // TODO: Make the API call and handle the results
+//             }
 
-        // Create the Test and add a button to the UI for running the test
-        const test = new Test();
-        const buttonContainer = document.getElementsByClassName('button-container')[0];
+//             setError(message) {
+//                 // TODO: Format the error
+//                 this.testResults.innerHTML = (message || '').toString();
+//             }
 
-        addButtonForTest(buttonContainer, test);
+//             setResults(results) {
+//                 // TODO: Format the results
+//                 this.testResults.innerHTML = (results || '').toString();
+//             }
+//         }
+
+//            function addButtonForTest(context, test) {
+//             let testButton = document.createElement('button');
+
+//             testButton.type = 'button';
+//             testButton.innerText = 'Get the Nashville Weather';
+//             testButton.onclick = () => test.run();
+
+//             context.appendChild(testButton);
+
+//             return testButton;
+//         }
+
+//         // Create the Test and add a button to the UI for running the test
+//         const test = new Test();
+//         const buttonContainer = document.getElementsByClassName('button-container')[0];
+
+//         addButtonForTest(buttonContainer, test);
+//API call
+// http://api.openweathermap.org/data/2.5/weather?APPID=25e989bd41e3e24ce13173d8126e0fd6&q=chicago
